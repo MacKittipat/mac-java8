7f6db88bd397748f4181fabe886290f2dc58f65f
mac <mac.kittipat@gmail.com>
2018-10-07 11:33:16 +0700

    Init project

---
+++ src/test/java/com/mackittipat/java8/AppTest.java
@@ -0,0 +1,13 @@
+package com.mackittipat.java8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
